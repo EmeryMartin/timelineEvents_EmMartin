2001e6bec35114a1d531744afb55967421737cab
Emery <144450140+EmeryMartin@users.noreply.github.com>
2023-11-22 12:08:09 -0400

    added blinking light to trigger

---
+++ Assets/Scripts/triggerTimeline.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class triggerTimeline : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+        }
+    }
+}
